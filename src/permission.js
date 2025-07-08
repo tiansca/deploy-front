fa3e0dac0f407145844daeeb6b9978f94bdf9d40
tianshicong <tianshicong@zbintel.net>
2025-07-08 19:37:19 +0800

    登录修复

--- src/permission.js
+++ src/permission.js
@@ -62,7 +62,7 @@ router.beforeEach(async(to, from, next) => {
           const VUE_CLI_AUTH_FRONTEND_URL = process.env.VUE_APP_AUTH_FRONTEND_URL
           if (error.response && error.response.status) {
             window.location.replace(VUE_CLI_AUTH_FRONTEND_URL + '/#/login?redirect=' + location.href)
-            console.log('111', VUE_CLI_AUTH_FRONTEND_URL + '/#/login?redirect=' + location.href)
+            console.log('111', VUE_CLI_AUTH_FRONTEND_URL + '?redirect=' + location.href)
           }
           NProgress.done()
         }
